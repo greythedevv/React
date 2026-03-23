5c22360dbf32fa31ed50038fa5f7dbd0f41b6e58
Greatness <Oluwolegreatnessadeola@gmail.com>
2026-03-23 18:04:21 +0100

    progress

--- component-project/src/App.jsx
+++ component-project/src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
+import { movies } from './movieComponent'
+import { MovieComponent } from './movieComponent'
 
 function App() {
 
   return (
     <>
-    
+    <movies/>
+    <MovieComponent/>
     </>
   )
 }
